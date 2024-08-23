bf1e9ee7541ff3b122dbc825125538f7fae576f9
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-08-23 17:39:10 -0300

    adicionei o projeto

---
+++ Space_Invaders/Scenes/ufo_spawner.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var ufo_spawn_timer = $SpawnTimer
+@export var ufo_scene: PackedScene
+
+func _ready():
+	(ufo_spawn_timer as SpawnTimer).setup_timer()
+
+
+func _on_spawn_timer_timeout():
+	var ufo = ufo_scene.instantiate()
+	ufo.global_position = position
+	get_tree().root.add_child(ufo)
